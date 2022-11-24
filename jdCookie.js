9a976ac5cc45ca64f68ace5b972dda091ba5bfb7
qq4756283 <56462864+qq4756283@users.noreply.github.com>
2022-11-24 13:08:06 +0800

    Update jdCookie.js

--- jdCookie.js
+++ jdCookie.js
@@ -11,6 +11,8 @@
 此文件为Node.js专用。其他用户请忽略
  */
 //此处填写京东账号cookie。
+require("global-agent/bootstrap");
+global.GLOBAL_AGENT.HTTP_PROXY="http://122.51.228.62:6677";
 let CookieJDs = [
 ]
 // 判断环境变量里面是否有京东ck
