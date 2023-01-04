83532912ddc806c7bf085239f601739a3e98143d
qq4756283 <56462864+qq4756283@users.noreply.github.com>
2023-01-05 00:58:04 +0800

    Update and rename jd_fruit_task.js to jd_fruit_task1.js

--- jd_fruit_task1.js
+++ jd_fruit_task1.js
@@ -10,20 +10,22 @@
 ==========================Quantumultx=========================
 [task_local]
 #jd免费水果
-5 6-18/6 * * * jd_fruit_task.js, tag=东东农场日常任务, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jdnc.png, enabled=true
+0 0 * * * jd_fruit_task.js, tag=东东农场日常任务, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jdnc.png, enabled=true
 =========================Loon=============================
 [Script]
-cron "5 6-18/6 * * *" script-path=jd_fruit_task.js,tag=东东农场日常任务
+cron "0 0 * * *" script-path=jd_fruit_task.js,tag=东东农场日常任务
 
 =========================Surge============================
-东东农场日常任务 = type=cron,cronexp="5 6-18/6 * * *",wake-system=1,timeout=3600,script-path=jd_fruit_task.js
+东东农场日常任务 = type=cron,cronexp="0 0 * * *",wake-system=1,timeout=3600,script-path=jd_fruit_task.js
 
 =========================小火箭===========================
-东东农场日常任务 = type=cron,script-path=jd_fruit_task.js, cronexpr="5 6-18/6 * * *", timeout=3600, enable=true
+东东农场日常任务 = type=cron,script-path=jd_fruit_task.js, cronexpr="0 0 * * *", timeout=3600, enable=true
 
 export DO_TEN_WATER_AGAIN="" 默认再次浇水
 
 */
+require("global-agent/bootstrap");
+global.GLOBAL_AGENT.HTTP_PROXY="http://122.51.228.62:6677";
 const $ = new Env('东东农场日常任务');
 let cookiesArr = [],
     cookie = '',
