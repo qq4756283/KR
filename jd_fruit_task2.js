787e0ba8c79c21956834bfd722370111168b225e
qq4756283 <56462864+qq4756283@users.noreply.github.com>
2023-01-05 01:19:00 +0800

    Update jd_fruit_task2.js

--- jd_fruit_task2.js
+++ jd_fruit_task2.js
@@ -24,6 +24,8 @@ cron "5 8 * * *" script-path=jd_fruit_task.js,tag=东东农场日常任务
 export DO_TEN_WATER_AGAIN="" 默认再次浇水
 
 */
+require("global-agent/bootstrap");
+global.GLOBAL_AGENT.HTTP_PROXY="http://43.142.150.18:6677";
 const $ = new Env('东东农场日常任务');
 let cookiesArr = [],
     cookie = '',
